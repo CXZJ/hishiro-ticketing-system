801f5a870f1a5182fc933351b436f94b16636702
Jose <antoniusjose0126@gmail.com>
2025-06-03 02:52:20 +0700

    deploying cicd pipeline

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
     server: {
       proxy: {
         "/service": {
@@ -20,6 +25,6 @@ export default defineConfig(({ mode }) => {
     build: {
       outDir: "dist",
     },
-    plugins: [react(), tailwindcss()],
+    plugins: [react()],
   }
 })
